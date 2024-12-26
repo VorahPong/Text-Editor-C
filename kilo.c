1329b1a1430d003c280c93f97928e3411229fc83
VorahPong <vorahpongmean@gmail.com>
2024-12-26 02:35:08 -0600

    Get size of the terminal

--- kilo.c
+++ kilo.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 /*** defines ***/
 
@@ -19,6 +20,8 @@
 
 /*** data ***/
 struct editorConfig {
+    int screenrows;
+    int screencols;
     struct termios original_termios;    // stored the original setting of terminal
 };
 
@@ -69,12 +72,23 @@ char editorReadKey() {
     return c;
 }
 
+int getWindowSize(int * rows, int * cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
 
 void editorDrawRows() {
     int y;
-    int row = 24;
-    for (y = 0; y < row; y++) {
+    for (y = 0; y < E.screenrows; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
@@ -104,8 +118,14 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+        die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
